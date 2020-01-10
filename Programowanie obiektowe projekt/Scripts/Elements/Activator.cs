6fb7e215c456f2a67ad3de5ad0742d61d63cd98e
KrzysztofWaleryszak <49519088+KrzysztofWaleryszak@users.noreply.github.com>
2020-01-10 18:35:23 +0100

    asd

---
+++ Programowanie obiektowe projekt/Scripts/Elements/Activator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+	public GameObject targer;
+	public Transform activator;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.name==activator.name)
+		{
+			targer.GetComponent<IActivable>().Activate();
+		}
+	}
+}
